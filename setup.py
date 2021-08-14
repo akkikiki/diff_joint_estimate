dcda640582ee9f2be89fbe8962bb1991e26df366
Yoshinari Fujinuma <fujinumay@gmail.com>
2021-08-14 13:07:08 -0600

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='diff_joint_estimate',
+    version='',
+    packages=['src', 'src.data', 'src.models', 'src.models.han', 'src.visualization'],
+    url='https://github.com/akkikiki/diff_joint_estimate',
+    license='',
+    author='fujinumay',
+    author_email='fujinumay@gmail.com',
+    description=''
+)
